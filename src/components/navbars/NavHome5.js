d24a32c1ef8cea2445da1e5529ef6e8c63cbe6ef
lwanga samson <samsonlwanga2@gmail.com>
2025-08-20 19:01:00 +0300

    riocommit

--- src/components/navbars/NavHome5.js
+++ src/components/navbars/NavHome5.js
@@ -4,10 +4,10 @@ const NavHome = ({ loginType, setLogin, setLoginType }) => {
         <div className='flex justify-around sticky top-0 bg-[#fff] w-full py-7 items-center'>
             <div className='flex justify-between ml-[-8rem]'>
                 <img src={Logo} alt='logo not visible' className='h-[60px] p-[8px] mx-4 cursor-pointer' />
-                <button className='mx-4'>Product</button>
-                <button className='mx-4'>Resources</button>
-                <button className='mx-4'>Pricing</button>
-                <button className='mx-4'>Talk to Sales</button>
+                <button className='mx-4'>GREAT AUBREY MEMORIAL COLLEGE</button>
+                <button className='mx-4'></button>
+                <button className='mx-4'>.</button>
+                <button className='mx-4'>.</button>
             </div>
             <div className='flex mr-[-8rem] justify-between'>
                 <button className='mx-4 hover:bg-black hover:text-[#fff] py-2 px-3 rounded-xl' onClick={() => {
@@ -20,7 +20,7 @@ const NavHome = ({ loginType, setLogin, setLoginType }) => {
                     setLoginType(false);
                     // window.location.reload();
                 }}>Sign up</button>
-                <button className='mx-4 text-[#fff] border-black border hover:bg-[#fff] hover:text-[black] bg-black py-2 px-3 rounded-xl'>Book a demo</button>
+                <button className='mx-4 text-[#fff] border-black border hover:bg-[#fff] hover:text-[black] bg-black py-2 px-3 rounded-xl'>GAMCO</button>
             </div>
         </div>
     );
